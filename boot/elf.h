6e334ba9e52537bc69302c3c5bf999061b13fddd
chainhelen <chainhelen@gmail.com>
2023-08-27 00:20:44 +0800

    Feat: load go elf

---
+++ boot/elf.h
@@ -0,0 +1,30 @@
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long long uint64;
+
+
+// Reference from https://sites.uclouvain.be/SystInfo/usr/include/linux/elf.h.html
+#define EI_NIDENT        16
+
+/* 64-bit ELF base types. */
+typedef uint64        Elf64_Addr;
+typedef uint16        Elf64_Half;
+typedef uint64        Elf64_Off;
+typedef uint32        Elf64_Word;
+
+typedef struct elf64_hdr {
+  unsigned char        e_ident[EI_NIDENT];        /* ELF "magic number" */
+  Elf64_Half e_type;
+  Elf64_Half e_machine;
+  Elf64_Word e_version;
+  Elf64_Addr e_entry;                /* Entry point virtual address */
+  Elf64_Off e_phoff;                /* Program header table file offset */
+  Elf64_Off e_shoff;                /* Section header table file offset */
+  Elf64_Word e_flags;
+  Elf64_Half e_ehsize;
+  Elf64_Half e_phentsize;
+  Elf64_Half e_phnum;
+  Elf64_Half e_shentsize;
+  Elf64_Half e_shnum;
+  Elf64_Half e_shstrndx;
+} Elf64_Ehdr;
--- boot/main.c
+++ boot/main.c
@@ -1,4 +1,6 @@
 #include "multiboot.h"
+#include "elf.h"
+
 unsigned char *videobuf = (unsigned char *)0xb8000;
 
 /* 参考 https://www.zhihu.com/question/49580321 */
@@ -6,11 +8,52 @@ unsigned char *videobuf = (unsigned char *)0xb8000;
 // the_cpu为每个cpu的数据，目前仅用于cpu栈
 char the_cpu[4096] __attribute__((section(".percpu")));
 void print(char *s, int len);
+void memcpy(char *dst, char *src, int count);
+uint64 loadGoELF(multiboot_info_t *info) ;
+uint64 loadELF(char *elf_header);
 
-void bp_main(unsigned long magic, multiboot_info_t *mbi)
+// The physical address of the multiboot header. On qemu for example this is typically at 0x9500.
+// TODO？ 这里函数调约是反的，可能跟直接进入64位有关
+void bp_main(multiboot_info_t *mbi, unsigned long magic)
 {
-    print("hello world", 11);
+    if(loadGoELF(mbi) == 1) {
+        print("1", 1);
+    } else {
+        print("0", 1);
+    }
     while(1){};
+}
+
+uint64 loadGoELF(multiboot_info_t *info) {
+    if (info->mods_count < 1) {
+        return 0;
+    }
+    multiboot_module_t *mod = (multiboot_module_t *)((uint64) (info->mods_addr));
+    int count = mod->mod_end - mod->mod_start + 1;
+    char *image = (char *)(100 << 20);
+    memcpy(image, (char *)((uint64)mod->mod_start), count);
+    return loadELF(image);
+}
+
+uint64 loadELF(char *elf_header) {
+    struct elf64_hdr *hdr = (struct elf64_hdr *)elf_header;
+    // magic 0x7f454C46
+    if (!(hdr->e_ident[0] == 0x7f &&
+            hdr->e_ident[1] == 0x45 &&
+            hdr->e_ident[2] == 0x4C &&
+            hdr->e_ident[3] == 0x46)) {
+       return 0;
+    }
+
+    return 1;
+}
+
+void memcpy(char *dst, char *src, int count) {
+    for (int i = 0;i < count;i++) {
+        *dst = *src;
+        dst++;
+        src++;
+    }
 }
 
 void print(char *s, int len)
